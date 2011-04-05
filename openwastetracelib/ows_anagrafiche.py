be22f10b5ac6718b6792920dc239907eceeb002e
imperius <imperius2@gmail.com>
2011-04-06 01:12:32 +0200

    change azienda

--- openwastetracelib/ows_anagrafiche.py
+++ openwastetracelib/ows_anagrafiche.py
@@ -14,29 +14,62 @@ Session = sessionmaker(bind=dbengine)
 session = Session()
 
 
+#Funzioni da implementare
 
-#Funsioni da implementare
+# owsAzienda
+# owsGetUpAzienda
+# owsGetUpSedeLegale
+# owsGetUpSedeSummary
 
-# owsGetAzienda
-# owsUpAzienda
 # owsUpAllAzienda
 #
-
-def owsGetAzienda (codiceFiscale):
+def owsAzienda (codiceFiscale):
     az, sl, sedesummary=wa.GetAzienda(codiceFiscale)
-    test=session.query(Azienda).filter (Azienda.codiceFiscale == codiceFiscale)
+    az=owsGetUpAzienda (az)
+    owsGetUpSedeLegale (az, sl)
+    owsGetUpSedeSummary (az, sedesummary)
+
+def owsGetUpAzienda (az):
+    #data un oggetto azienda restituisce l'oggetto azienda eventualmente aggiornato, si occupa del salvataggio sul db.
+    test=session.query(Azienda).filter (Azienda.codiceFiscale == az.codiceFiscale)
     if test.count()==0:
-        #non c'e' aggiungo
-        session.add(az)
+        #non c'e' --> aggiungo
+        aznew=az
+        session.add(aznew)
+        session.commit()
     else:
-        az=test.one()
+        #intanto per ora prendo per buono quello del database.
+        aznew=test.one()
+        #c'e' controlla il numero di versione
+        if az.versione!=test.one().versione:
+            #aggiorna il record, si copia il vecchio id sul nuovo, e sostituice l'oggetto
+            az.Aid=test.one().Aid
+            aznew=az
+            session.merge(aznew)
+            session.commit()
+    #in aznew c'e' sicuramente l'oggetto giusto
+    return aznew
 
 
-    az.RelSedeLegale.append(sl)
-    session.add(sl)
+def owsGetUpSedeLegale (az, sl):
+    #dato un oggetto sede legale e un azienda associata aggiorna eventualmente l'oggetto sede legale , si occupa del salvataggio su db e non restituisce niente.
+    test=session.query(SedeLegale).filter (SedeLegale.idSIS == sl.idSIS)
+    if test.count()==0:
+        az.RelSedeLegale.append(sl)
+        session.add(sl)
+    else:
+        sldb=test.one()
+        if sldb.versione!=sl.versione:
+            sl.Slid=sldb.Slid
+            sldb=sl
+            #non serve appende perche' e' gia appeso
+            #az.RelSedeLegale.append(sldb)
+            session.merge(sldb)
+    session.commit()
 
+def owsGetUpSedeSummary (az, sedesummary):
+    #data un azienda e una sedesummary (che e' una lista di sedi), aggiorna le sedi e si occupa del saltaggio sul db.
     for sede1 in sedesummary:
         az.RelSedi.append(sede1)
         session.add(sede1)
 
-    session.commit()
